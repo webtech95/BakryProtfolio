910b234c453260af5b1d713602120df81079dc88
webtech95 <webtechhub95@gmail.com>
2025-04-12 11:33:00 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
 root.render(
   <React.StrictMode>
     <App />
